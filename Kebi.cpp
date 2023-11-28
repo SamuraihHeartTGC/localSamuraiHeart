558962683aaaced8ed6da2c2769e37d035c44573
SamuraihHeartTGC <152171789+SamuraihHeartTGC@users.noreply.github.com>
2023-11-28 12:24:36 +0800

    RemakeKebi

---
+++ Kebi.cpp
@@ -0,0 +1,33 @@
+﻿#include "Debug.h"
+#ifdef KeBi
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+    cout << "扣1复活科比:";
+    Mat img;
+    img = imread("kebi.jpg");
+    if (img.empty())
+    {
+        cout << "请确认图像文件名是否正确" << endl;
+        return -1;
+    }
+    int num;
+    cin >> num;
+
+
+    imshow("test", img);
+
+    cout << "孩子们我回来啦~";
+    waitKey(0);
+    return 0;
+}
+
+
+#endif
